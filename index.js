e57a2fe2fff9606230cae6f1621fb1ec4bf99dcb
Drew Overcash <drewovercash@gmail.com>
2017-11-04 22:11:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-function add(){
+function add(a, b){
   console.log(a+b);
 };
+
 function subtract(){
   console.log(a-b);
 };
+
 function multiply(){
   console.log(a*b);
 };
+
 function divide(){
   console.log(a/b);
 };
